e3a257d11341e7d6a9b94de06a8d4251b01057d3
Default <shalpha_2@naver.com>
2022-11-15 00:13:46 +0900

    2 variables

---
+++ 2.variables/v.js
@@ -0,0 +1,15 @@
+'use strict' // 엄격모드 적용 > js 단점 보안
+
+//let 변수 연습
+let openBox = "강아지";
+console.log(openBox);
+
+openBox = "고양이";
+console.log(openBox);
+// 이름이 같은 변수는 생성 불가 >  let openBox = "다람쥐";
+
+//const 변수 연습
+const foodBox = "쿠키";
+console.log(foodBox);
+
+// 변수 내용 변경 불가 >  foodBox = "약과";
